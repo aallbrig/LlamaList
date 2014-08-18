9ba176e25162f91369e5ca8ee194cae5abfa1fb3
Andrew Allbright <aallbrig@Andrews-MacBook-Pro.local>
2014-08-18 08:55:18 -0400

    Llama List first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+app.get('/hello.txt', function(req, res){
+    res.send('Hello World');
+});
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+app.get('/home', function(req, res){
+  res.sendfile('home.html');
+});
+app.get('/search', function(req, res){
+  res.sendfile('search.html');
+})
+
+var server = app.listen(4000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
